8f414b673c90bbde4bc0c452313b78e7ae3f056b
mahabubmonir339 <mahabubhossenmonir@gmail.com>
2022-04-05 05:42:39 +0600

    blogs update

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className="container mb-5 mt-5 leading-tight">
             <h2>Qstion No 1. what is context api?</h2>
-            <h3>Ans: <h5>Context API is an idea to produce global variables.
+            <h3>Ans: </h3><p>Context API is an idea to produce global variables.
                 These variables then can be passed around within the application.
                 Any component that requires to access the variables AKA “state” can
                 easily be done with the help of Context API.
@@ -14,10 +14,10 @@ const Blogs = () => {
                 any React application.We often need to pass our state across components.
                 Sometimes what happens is that we need to pass state in multiple
                 components in the application.
-            </h5></h3>
+            </p>
 
             <h2>Qstion No 2. what is semantic tag?</h2>
-            <h3>Ans: <h5>Although nearly every HTML4 tag and all the HTML5 tags have
+            <h3>Ans:</h3> <p>Although nearly every HTML4 tag and all the HTML5 tags have
                 semantic meanings, some tags are primarily semantic.Semantic HTML or
                 semantic markup is HTML that introduces meaning to the web page rather
                 than just presentation. For example, a "p" tag indicates that the enclosed
@@ -27,7 +27,7 @@ const Blogs = () => {
                 On the flip side of this equation, tags such as "b" and "i" are not semantic.
                 They define only how the text should look (bold or italic), and don't provide
                 any additional meaning to the markup.
-            </h5></h3>
+            </p>
         </div>
     );
 };
