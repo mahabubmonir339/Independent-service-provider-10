1ac6f1baa7c4526aaa578d97d73a8e9e3aeca5e4
mahabubmonir339 <mahabubhossenmonir@gmail.com>
2022-04-05 16:05:15 +0600

    dashboard add

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,7 +6,12 @@ import {
     XAxis,
     YAxis,
     CartesianGrid,
-    Tooltip
+    Tooltip,
+    ComposedChart,
+    Legend,
+    Bar,
+    Line,
+    Scatter
 } from "recharts";
 
 const data = [
@@ -50,49 +55,79 @@ const data = [
 
 export default function App() {
     return (
-        <AreaChart
-            width={1500}
-            height={600}
-            data={data}
-            margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 0
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area
-                type="monotone"
-                dataKey="month"
-                stackId="1"
-                stroke="#8884d8"
-                fill="#8884d8"
-            />
-            <Area
-                type="monotone"
-                dataKey="investment"
-                stackId="1"
-                stroke="#82ca9d"
-                fill="#82ca9d"
-            />
-            <Area
-                type="monotone"
-                dataKey="sell"
-                stackId="1"
-                stroke="#ffc658"
-                fill="#ffc658"
-            />
-            <Area
-                type="monotone"
-                dataKey="revenue"
-                stackId="1"
-                stroke="#ffc658"
-                fill="#ffc658"
-            />
-        </AreaChart>
+
+        <> <h1>Chart</h1>
+            <div className="container d-flex">
+                <div>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area
+                            type="monotone"
+                            dataKey="month"
+                            stackId="1"
+                            stroke="#8884d8"
+                            fill="#8884d8"
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="investment"
+                            stackId="1"
+                            stroke="#82ca9d"
+                            fill="#82ca9d"
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="sell"
+                            stackId="1"
+                            stroke="#ffc658"
+                            fill="#ffc658"
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="revenue"
+                            stackId="1"
+                            stroke="#ffc658"
+                            fill="#ffc658"
+                        />
+                    </AreaChart>
+                </div>
+                <div>
+                    <ComposedChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 20,
+                            bottom: 20,
+                            left: 20
+                        }}
+                    >
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Area type="monotone" dataKey="month" fill="#8884d8" stroke="#8884d8" />
+                        <Bar dataKey="investment" barSize={20} fill="#413ea0" />
+                        <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                        <Scatter dataKey="sell" fill="red" />
+                    </ComposedChart>
+                </div>
+            </div>
+        </>
     );
 }
